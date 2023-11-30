3bcaf4575d6cd268263548115c4bb08662204b9a
Julian <juwalu02@gmail.com>
2023-11-29 16:10:49 -0800

    add Twitter API files

---
+++ Twitter_Handler.py
@@ -0,0 +1,35 @@
+import tweepy
+from auth import (
+    api_key,
+    api_secret,
+    access_token,
+    bearer_token,
+    access_token_secret
+)
+from Json_Parser import Json_Parser
+
+
+class Twitter_Handler(object):
+
+    client: tweepy.Client
+    api: tweepy.API
+
+    def __init__(self):
+        self.client = tweepy.Client(
+            bearer_token,
+            api_key,
+            api_secret,
+            access_token,
+            access_token_secret
+        )
+        auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+        self.api = tweepy.API(auth)
+
+    def send_tweet(self, message):
+        self.client.create_tweet(text = message)
+
+# FOR TESTING ONLY
+if __name__ == '__main__':
+    twitter = Twitter_Handler()
+    twitter.send_tweet("Hello")
+# ================
